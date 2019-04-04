cf50d6fede17924ef807cf40cd158e9ad0f9ecda
cfsterpka <7240312+cfsterpka@users.noreply.github.com>
2019-04-04 10:23:12 -0400

    Add files via upload
    sync my files

--- LIM_scripts/examples/histogram_planewave_fits.py
+++ LIM_scripts/examples/histogram_planewave_fits.py
@@ -5,36 +5,57 @@ from LoLIM.make_planewave_fits import planewave_fits
 from LoLIM.IO.raw_tbb_IO import  filePaths_by_stationName
 from LoLIM.utilities import v_air
 
+from LoLIM.definitions import definitions
+D = definitions()
+
+import numpy as np
+
 ## these lines are anachronistic and should be fixed at some point
 from LoLIM import utilities
-utilities.default_raw_data_loc = "/exp_app2/appexp1/public/raw_data"
-utilities.default_processed_data_loc = "/home/brian/processed_files"
+utilities.default_raw_data_loc = D.default_raw_data_loc
+utilities.default_processed_data_loc = D.default_processed_data_loc
+
+maxs = {}
 
 if __name__ == "__main__":
-    
-    timeID = "D20180813T153001.413Z"
+
+    timeID = D.timeID
     stations = filePaths_by_stationName(timeID)
-    
-    for sname in ['RS503']:#['RS306','RS106', "RS310", "RS305"]:#stations.keys():
-        
+
+    for sname in ['CS028', 'CS301', 'RS106', 'RS306' ]: #stations.keys():
+     if sname not in D.bad_stations:
+
         print("processing", sname)
-        
-        RMSs, zeniths, azimuths = planewave_fits(timeID = timeID, 
-                       station = sname, 
-                       polarization  = 0, 
-                       initial_block = 2500, 
-                       number_of_blocks = 1000, 
-                       pulses_per_block = 100, 
-                       pulse_length = 50 + int(100/(v_air*5.0E-9)), 
-                       min_amplitude = 50, 
-                       upsample_factor = 4, 
+
+        RMSs, zeniths, azimuths = planewave_fits(timeID = timeID,
+                       station = sname,
+                       polarization  = 0,
+                       initial_block = 2500,
+                       number_of_blocks = 1000,
+                       pulses_per_block = 10,
+                       pulse_length = 50 + int(100/(v_air*5.0E-9)),
+                       min_amplitude = 50,
+                       upsample_factor = 4,
                        min_num_antennas = 4,
                        verbose = False, ## doesn't do anything anyway
-                        polarization_flips="polarization_flips.txt", bad_antennas="bad_antennas.txt", additional_antenna_delays = "ant_delays.txt",  
+                        polarization_flips="polarization_flips.txt", bad_antennas="bad_antennas.txt", additional_antenna_delays = "ant_delays.txt",
                         positive_saturation = 2046, negative_saturation = -2047, saturation_post_removal_length = 50, saturation_half_hann_length = 50)
-        
+
+
         print(len(RMSs), "found planewaves")
-        plt.hist(RMSs, bins=50, range=[0,10e-9])
-        plt.show()
-    
-    
+        plt.title(sname)
+        histy, histx, _ = plt.hist(RMSs, bins=100, range=[0.0,10e-9])
+        if RMSs.size > 0:
+          #m = max(histy)
+          m = histy.max()
+          i = np.where(histy==m)
+          mx = histx[i][0]
+          print("Maximum at: "+str(mx))
+          maxs[sname] = mx
+        plt.savefig("./hist/"+sname+".png")
+        plt.close()
+        #plt.show()
+
+    print("Maximums: ")
+    for m in maxs:
+        print(m,maxs[m])
--- LIM_scripts/examples/run_findRFI.py
+++ LIM_scripts/examples/run_findRFI.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-from LoLIM.utilities import processed_data_dir, log
+from LoLIM.utilities import processed_data_dir, logger
 from LoLIM.IO.raw_tbb_IO import MultiFile_Dal1, filePaths_by_stationName
 from LoLIM.findRFI import FindRFI
 
@@ -15,50 +15,56 @@ from os import mkdir
 from os.path import isdir
 from pickle import dump
 
+from LoLIM.definitions import definitions
+D = definitions()
+
+
 ## these lines are anachronistic and should be fixed at some point
 from LoLIM import utilities
-utilities.default_raw_data_loc = "/exp_app2/appexp1/public/raw_data"
-utilities.default_processed_data_loc = "/home/brian/processed_files"
+utilities.default_raw_data_loc = D.default_raw_data_loc
+utilities.default_processed_data_loc = D.default_processed_data_loc
+
+log = logger()
 
 if __name__ == "__main__":
-    timeID = "D20180813T153001.413Z"
+    timeID = D.timeID
     output_folder = "/findRFI"
     out_fname = "/findRFI_results"
-    block_size = 2**16
-    initial_block = 1000
+    block_size = D.block_size
+    initial_block = 3000
     num_blocks = 20
     max_blocks = 500
-    
-    skip_stations = ["CS401"]
-    
-    
+
+    skip_stations = []#D.bad_stations
+
+
     processed_data_dir = processed_data_dir(timeID)
-    
+
     output_fpath = processed_data_dir + output_folder
     if not isdir(output_fpath):
         mkdir(output_fpath)
-        
+
     log.set(output_fpath+'/log.txt', True)
     log.take_stdout()
-        
-    
+
+
     #### get paths to raw data by station ####
     raw_fpaths = filePaths_by_stationName(timeID)
-    
+
     output = {}
-    
+
     for station in raw_fpaths.keys():
         if station in skip_stations:
             continue
         print("station", station)
-        
+
         path = output_fpath + '/' + station
         if not isdir(path):
             mkdir(path)
-            
+
         TBB_data = MultiFile_Dal1( raw_fpaths[station] )
         out = FindRFI(TBB_data, block_size, initial_block, num_blocks, max_blocks, verbose=True, figure_location=path)
         output[station] = out
-        
+
     with open(output_fpath+out_fname, 'wb') as fout:
         dump(output, fout)
